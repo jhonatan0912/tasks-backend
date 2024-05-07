caf1751dde351318f7d52fe594f8de408145e334
Jhonatan <jhoonatan_0912@hotmail.com>
2024-05-06 23:27:48 -0500

    Add Relations/ User => Tasks

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '@auth/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Task {
@@ -27,4 +28,7 @@ export class Task {
     default: false,
   })
   done: boolean;
+
+  @ManyToOne(() => User, user => user.tasks)
+  user: User;
 }
